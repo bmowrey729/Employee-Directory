2aae4d34bc83b06c24b9f6484553724123601c23
b729 <mowrey729@gmail.com>
2021-06-18 15:00:51 -0500

    Rebuilt core files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
-import React from 'react';
-import List from './components/List';
-
-const groceries = [
-  {
-    id: 1,
-    name: 'Milk',
-    purchased: true,
-  },
-  {
-    id: 2,
-    name: 'Eggs',
-    purchased: true,
-  },
-  {
-    id: 3,
-    name: 'Cheese',
-    purchased: false,
-  },
-  {
-    id: 4,
-    name: 'Cake Mix',
-    purchased: false,
-  },
-  {
-    id: 5,
-    name: 'Carrots',
-    purchased: false,
-  },
-  {
-    id: 6,
-    name: 'Juice',
-    purchased: true,
-  },
-];
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <List groceries={groceries} />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;

a63408bac02fff0faa562b52cc88b7631f75fe9b
b729 <mowrey729@gmail.com>
2021-06-18 12:39:01 -0500

    Initial commit React componets added

---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
+function List(props) {
+  return (
+    <ul className='list-group'>
+      {props.groceries.map((item) => (
+        <li className='list-group-item' key={item.id}>
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+<p>1234</p>;
+
+export default List;
